e429a9e4082187c71914969c57cb299c59dfb7c4
EduardRzhavski <142872678+EduardRzhavski@users.noreply.github.com>
2023-11-22 20:38:41 +0200

    3.2

---
+++ 3.2/src/App.jsx
@@ -0,0 +1,43 @@
+// import React from 'react';
+// import MyComponent from './MyComponent';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Your React App</h1>
+//       <MyComponent />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// 
+
+// import React from 'react';
+// import NumberComponent from './NumberComponent'; 
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Your React App</h1>
+//       <NumberComponent />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// 
+
+import React from 'react';
+import StringLengthComponent from './StringLengthComponent';
+const App = () => {
+  return (
+    <div>
+      <h1>Your React App</h1>
+      <StringLengthComponent />
+    </div>
+  );
+}
+
+export default App;
