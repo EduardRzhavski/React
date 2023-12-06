338583d9f1dae522483228e56177c6a8c9be4010
EduardRzhavski <142872678+EduardRzhavski@users.noreply.github.com>
2023-12-06 10:46:26 +0200

    19.2.2

--- 19.2/19.2/src/Page1.jsx
+++ 19.2/19.2/src/Page1.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useTheme } from './ThemeContext';
+import './Page.css';
 
 const Page1 = () => {
+  const { isNightMode } = useTheme();
+
   return (
-    <div className="page">
+    <div className={`page ${isNightMode ? 'night-mode' : 'day-mode'}`}>
       <h2>Page 1</h2>
       <p>This is the content of Page 1.</p>
     </div>
--- 19.2/19.2/src/Page2.jsx
+++ 19.2/19.2/src/Page2.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useTheme } from './ThemeContext';
+import './Page.css';
 
 const Page2 = () => {
+  const { isNightMode } = useTheme();
+
   return (
-    <div className="page">
+    <div className={`page ${isNightMode ? 'night-mode' : 'day-mode'}`}>
       <h2>Page 2</h2>
       <p>This is the content of Page 2.</p>
     </div>
